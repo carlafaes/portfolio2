2060f1f71770f7c441e8918c66afc1dadcf402c4
carlafaes <carlafaes18@hotmail.com>
2022-04-29 18:58:04 -0300

    contact

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -5,8 +5,15 @@ import Mailer from './Mailer'
 export default function Contact(){
 
     return(
-        <div id='contact' className={style.container}>
+        <div className={style.container_principal}>
+            <div id='contact' className={style.container}>
+            <div className={style.container_text}>
+                <p>I am interested in obtaining a job in which I can continue to professionalize and learn day by day, I enjoy working in a team. However, if you have any other request or question, please feel free to use the form below.</p>
+            </div>
             <Mailer/>
         </div>
+        <p>FOLLOW ME</p>
+        </div>
+
     )
 }
