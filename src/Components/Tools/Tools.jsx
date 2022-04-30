f1d94f98f575a3d5efdcc6e57bd6dcb01a319640
carlafaes <carlafaes18@hotmail.com>
2022-04-30 11:04:44 -0300

    tools

---
+++ src/Components/Tools/Tools.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import js from './tools/js.png';
+import css from './tools/css.png';
+import git from './tools/git.png';
+import html from './tools/html.png';
+import sass from './tools/sass.png';
+import express from './tools/express1.png';
+import Postgresql from './tools/postgresql.png';
+import react from './tools/react.png';
+import redux from './tools/redux.png';
+import sequelize from './tools/sequelize.png';
+import threejs from './tools/3js.png';
+import framer from './tools/framer.png';
+
+const Tools=[
+    {name:sass, link:'https://www.javascript.com/',description:'SCSS'},
+    {name:html, link:'https://www.javascript.com/',description:'HTML5'},
+    {name:threejs, link:'https://www.javascript.com/',description:'Three Js'},
+    {name:git, link:'https://www.javascript.com/',description:'git'},
+    {name:js, link:'https://www.javascript.com/',description:'Javascript'},
+    {name:css, link:'#',description:'CSS3'},
+    {name:express, link:'https://expressjs.com/es/',description:'Express Js'},
+    {name:formik, link:'https://formik.org/',description:'Formik'},
+    {name:materialui, link:'https://mui.com/',description:'Material-UI'},
+    {name:node, link:'https://nodejs.org/es/',description:'Node Js'},
+    {name:Postgresql, link:'https://www.postgresql.org/',description:'PostgreSQL'},
+    {name:react, link:'https://es.reactjs.org/',description:'React'},
+    {name:redux, link:'https://es.redux.js.org/',description:'Redux'},
+    {name:sequelize, link:'https://sequelize.org/ ',description:'Sequelize'},
+    {name:framer, link:'https://www.framer.com/motion/',description:'Framer-Motion'},
+
+]
+
+
