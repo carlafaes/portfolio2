d67d85d6a6e35fb3b5df78ad79319678ec654566
carlafaes <carlafaes18@hotmail.com>
2022-04-30 15:05:02 -0300

    motion en progreso

--- src/Components/Tools/Tools.jsx
+++ src/Components/Tools/Tools.jsx
@@ -4,7 +4,7 @@ import css from './tools/css3.png';
 import git from './tools/git.png';
 import html from './tools/html.png';
 import sass from './tools/sass.png';
-import express from './tools/express3.png';
+import express from './tools/express2.png';
 import Postgresql from './tools/postgresql.png';
 import react from './tools/react.png';
 import redux from './tools/redux.png';
@@ -14,6 +14,7 @@ import framer from './tools/framer.png';
 import formik from './tools/formik.png';
 import node from './tools/nodejs.png';
 import style from './Tools.module.css'
+import { motion } from "framer-motion";
 
 const Tools=[
     {name:sass, link:'https://sass-lang.com/',description:'SCSS'},
@@ -22,7 +23,7 @@ const Tools=[
     {name:git, link:'https://git-scm.com/',description:'Git'},
     {name:js, link:'https://www.javascript.com/',description:'Javascript'},
     {name:css, link:'#',description:'CSS3'},
-    // {name:express, link:'https://expressjs.com/es/',description:'Express Js'},
+    {name:express, link:'https://expressjs.com/es/',description:'Express Js'},
     {name:formik, link:'https://formik.org/',description:'Formik'},
     {name:node, link:'https://nodejs.org/es/',description:'Node Js'},
     {name:Postgresql, link:'https://www.postgresql.org/',description:'PostgreSQL'},
@@ -41,14 +42,23 @@ export default function Tool(){
                 <h3 className={style.title}>Skills</h3>
                 <p className={style.parr}>These are the technologies I have used in the projects I have done so far.</p>
             </div>
-            <div className={style.container_tools}>
+            <div
+
+            className={style.container_tools}>
                 {Tools ? Tools.map((tec,index)=>(
-                    <div key={Math.random(index)}>
+                    <motion.div
+                    whileHover={{
+                        scale: [1, 2, 2, 1, 1],
+                        borderRadius: ["20%", "20%", "40%", "40%", "20%"],
+                      }}
+                      whileTap={{ scale: 0.9 }}
+                    key={Math.random(index)}>
                     <a  href={tec.link}>
-                        <img className={style.img} src={tec.name} alt='tec'/>
-                        <p>{tec.description}</p>
+                        <img
+                        className={style.img} src={tec.name} alt='tec'/>
+                        <p className={style.p_}>{tec.description}</p>
                     </a>
-                    </div>
+                    </motion.div>
                 )):
                 <p>loading</p>}
             </div>
